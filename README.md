7e6e1c33e3e3e35c1524a4a62c317028d4adb7c1
Chris Zehner <cbzehner@gmail.com>
2020-04-04 13:32:41 -0700

    Example of pass-by-reference issue in Pong tutorial

---
+++ README.md
@@ -0,0 +1,1 @@
+https://tetra.seventeencups.net/tutorial/
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use tetra::graphics::{self, Color, Texture};
+use tetra::input::{self, Key};
+use tetra::math::Vec2;
+use tetra::{Context, ContextBuilder, State};
+
+const WINDOW_WIDTH: f32 = 640.0;
+const WINDOW_HEIGHT: f32 = 480.0;
+const PADDLE_SPEED: f32 = 8.0;
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
+        .quit_on_escape(true)
+        .build()?
+        .run(GameState::new)
+}
+
+struct GameState {
+    paddle_texture: Texture,
+    paddle_position: Vec2<f32>,
+}
+
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        let paddle_texture = Texture::new(ctx, "./resources/player1.png")?;
+        let paddle_position =
+            Vec2::new(16.0, (WINDOW_HEIGHT - paddle_texture.height() as f32) / 2.0);
+
+        Ok(GameState {
+            paddle_texture,
+            paddle_position,
+        })
+    }
+}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        graphics::draw(ctx, &self.paddle_texture, &self.paddle_position);
+
+        Ok(())
+    }
+}
